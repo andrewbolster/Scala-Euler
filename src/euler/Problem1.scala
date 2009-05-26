a6656fff2017dfd7f16812b1e9863725f8a85198
devuser <devuser@vmubuntu>
2009-05-26 21:31:14 +0100

    Heading for generalised solution for list of (N factors) to lim-> max

--- src/euler/Problem1.scala
+++ src/euler/Problem1.scala
@@ -1,5 +1,11 @@
-package euler
 
-class Problem1 {
-
+object Problem1 {
+    
+	def isFactor(xs: List[Int], x: Int ) = xs.exists( fac => x % fac == 0)
+	
+	def solution(xs: List[Int], max: Int ) = ( 1 until max ).filter( n => isFactor(xs,n)).foldLeft(0)(_+_)
+	
+	def main (args: Array[String]) {
+		print("Answer: ",solution(List(3,5),1000))
+	}
 }
