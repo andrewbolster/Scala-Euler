f2af31648985f62b744653e528fa3d23a16b644a
andrewbolster <andrew.bolster@gmail.com>
2009-05-27 07:55:17 -0700

    renamed variables to make more sense (f = factor currently used from fs, n = integer currently being filtered)

--- src/euler/Problem1.scala
+++ src/euler/Problem1.scala
@@ -1,5 +1,5 @@
 object Problem1 {
-	def solution(fs: List[Int], max: Int ) = ( 1 until max ).filter( f => fs.exists( n => f % n == 0)).foldLeft(0)(_+_)
+	def solution(fs: List[Int], max: Int ) = ( 1 until max ).filter( n => fs.exists( f => n % f == 0)).foldLeft(0)(_+_)
 	def main (args: Array[String]) {
 	  print("Answer: ",solution(List(3,5),1000))
 	}
